3fc5a642467cc8adc19c3aef0cc7273f6adc41f0
Ferdiansyah-Juliputra <ferdiansyah130204@gmail.com>
2025-09-17 20:49:23 +0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -169,9 +169,9 @@ export default function Home() {
                 <div className="bg-sand p-8 rounded-2xl">
                   <h3 className="font-bold text-xl sm:text-2xl text-mahogany mb-4">Social Media</h3>
                   <div className="flex flex-col space-y-4">
-                    <a href="#" target='blank' className="text-mahogany hover:underline transition duration-200 ease-in-out flex items-center">
+                    <a href="https://www.instagram.com/restyle.id06?utm_source=qr&igsh=MW83eDIzNWN6MnNjdg==" target='blank' className="text-mahogany hover:underline transition duration-200 ease-in-out flex items-center">
                       <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="mr-3"><rect width="20" height="20" x="2" y="2" rx="5" ry="5"/><path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"/><line x1="17.5" x2="17.51" y1="6.5" y2="6.5"/></svg>
-                      @ReStyle.ID
+                      @restyle.id06
                     </a>
                     <a href="#" target='blank' className="text-mahogany hover:underline transition duration-200 ease-in-out flex items-center">
                       <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="mr-3"><path d="M6 2 3 6v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V6l-3-4Z"/><path d="M3 6h18"/><path d="M16 10a4 4 0 0 1-8 0"/></svg>
